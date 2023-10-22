603109bd43c9e87de4455ca2723296b9a8da3ad2
Tahsin2k5 <tahsin2k5.com>
2023-10-22 11:37:17 +0600

    random 1

--- Shahajjo/shahajjo/views.py
+++ Shahajjo/shahajjo/views.py
@@ -7,30 +7,17 @@ def index(request):
     return render(request,'admin/cat.html')
 def insert(request):
     cat_name = request.POST.get('name')
+    gender = request.POST.get('gender')
+    phn_number = request.POST.get('phn_number')
+    occupation = request.POST.get('occupation')
+    religion = request.POST.get('religion')
+    gender = request.POST.get('gender')
+    bank_no = request.POST.get('bank_no')
 
-    if cat_name:
-        cat_obj = Categories()
-        cat_obj.name = cat_name
-        cat_obj.save()
-        messages.success(request, "Data inserted successfully")
-
-        
-    else:
-        messages.success(request, "The field cannot be empty")
-        
-
-
-    # gender = request.POST.get('gender')
-    # phn_number = request.POST.get('phn_number')
-    # occupation = request.POST.get('occupation')
-    # religion = request.POST.get('religion')
-    # gender = request.POST.get('gender')
-    # bank_no = request.POST.get('bank_no')
-
-    # cat_obj = Categories()
-    # cat_obj.name = cat_name
-    # cat_obj.save()
-    # messages.success(request, "Data inserted successfully")
+    cat_obj = Categories()
+    cat_obj.name = cat_name
+    cat_obj.save()
+    messages.success(request, "Data inserted successfully")
     return redirect('catadmin')
 
     
